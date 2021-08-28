dc4e1f298767c917bfa59026bc3d7d0214dc2864
lzambrano94684 <lzambrnop@miumg.edu.gt>
2021-08-27 23:09:41 -0600

    tarea6

--- Test.java
+++ Test.java
@@ -4,10 +4,10 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Metodos metodos = new Metodos("Bryan Recinos", "2009354654");
+        Metodos metodos = new Metodos("Alvaro de la Cruz", "99999999");
         metodos.persona();
-        Metodos metodos1 = new Metodos("1 Avenida Villa Nueva");
-        metodos1.persona("451");
+        Metodos metodos1 = new Metodos("Zona 1 Avenida Villa Nueva");
+        metodos1.persona("666");
 
     }
 }
